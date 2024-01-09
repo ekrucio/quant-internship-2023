26219a134ed070dc1230c7e66af52cad5ac64bfb
github-action-benchmark <github@users.noreply.github.com>
2024-01-09 11:49:22 +0000

    add My Project Go Benchmark (go) benchmark result for df8a55b7455de8336a270da52139eb3277044a7e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704800355101,
+  "lastUpdate": 1704800962365,
   "repoUrl": "https://github.com/ekrucio/quant-internship-2023",
   "entries": {
     "My Project Go Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 240594,
             "unit": "ns/op",
             "extra": "4494 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kycenstein@gmail.com",
+            "name": "Krastan Stoyanov",
+            "username": "ekrucio"
+          },
+          "committer": {
+            "email": "kycenstein@gmail.com",
+            "name": "Krastan Stoyanov",
+            "username": "ekrucio"
+          },
+          "distinct": true,
+          "id": "df8a55b7455de8336a270da52139eb3277044a7e",
+          "message": "oops",
+          "timestamp": "2024-01-09T13:48:45+02:00",
+          "tree_id": "30845fd6b59fc876faadd1a6d4d3a86830ac44ae",
+          "url": "https://github.com/ekrucio/quant-internship-2023/commit/df8a55b7455de8336a270da52139eb3277044a7e"
+        },
+        "date": 1704800962030,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGetGifsHandler",
+            "value": 241245,
+            "unit": "ns/op",
+            "extra": "4914 times\n4 procs"
           }
         ]
       }
